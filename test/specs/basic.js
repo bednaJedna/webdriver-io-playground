685bbcfe0167cb3749435344aa696ade22fb0ebf
bednaJedna <bednarik.radek@gmail.com>
2019-10-09 15:22:32 +0200

    init

---
+++ test/specs/basic.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect
+
+describe('Skoda-Auto homepage', () => {
+  before(function () {
+    browser.url('https://www.skoda-auto.cz')
+  })
+
+  it('should have the right title', () => {
+    const title = browser.getTitle()
+    expect(title).to.be.a('ŠKODA AUTO Česká republika | Oficiální web‎ ŠKODA AUTO a.s.')
+  })
+
+  it('should have the cookie notice popup iframe present', () => {
+    const cookie_iframe = $('//iframe[@scrolling="no"]')
+    cookie_iframe.waitForExist(5000)
+    expect(cookie_iframe.isExisting()).to.be.true
+  })
+})
