bf84c8a467bea1dc499e65d4b14312ddced47761
bednaJedna <bednarik.radek@gmail.com>
2019-10-11 14:38:34 +0200

    paths!!!

--- test/specs/homepage.ts
+++ test/specs/homepage.ts
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var HomePage = require('../../pageobjects/home.page').Page;
+var HomePage = require('./test/pageobjects/home.page').Page;
 
 describe('Homepage tests', () => {
     it('datalayer is present', () => {
